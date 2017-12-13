3d5421dcea5c4a2baa12133d28ac992bb7e8940a
nico24687 <theonlynico@hotmail.com>
2017-12-12 20:22:44 -0700

    created category table with validations

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe "validations" do 
+    it "has a valid factory" do 
+      category = build(:category)
+
+      expect(category).to be_valid
+    end 
+    it "requires a title" do 
+      category = build(:category, title: "")
+
+      expect(category).not_to be_valid
+    end 
+  end 
+end
